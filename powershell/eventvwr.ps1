9c675baa52a593e892645af65290e527bf4abb7d
ZehnMilliarden <git@lxf.science>
2022-06-07 17:18:20 +0800

    【eventvwr.ps1】基本枚举筛选逻辑实现

--- powershell/eventvwr.ps1
+++ powershell/eventvwr.ps1
@@ -1,45 +1,254 @@
-# windows äº‹ä»¶æŸ¥çœ‹å™¨
+# windows ÈÕÖ¾·ÖÎö
 
-function Text
+class CNtLogEventFilter
 {
-    param
-    (
-        [System.Object]$TestTxt
-    )
+    hidden $_FilterTable = @{}
+    
+    [bool]IsTarget([System.Object] $target)
+    {
+        $Script:Key = $target.Name
+        $Script:Value = $target.Value
 
-    Write-Host $TestTxt.ToString()
+        if ($Script:Key -eq "Logfile")
+        {
+            return $true;
+        }
+
+        return $this._IsTarget($Script:Key, $Script:Value)
+    }
+
+    hidden [bool] _IsTarget([string]$key, $value)
+    {
+
+        $Script:bRet = $true;
+
+        do {
+        
+            if ($this._FilterTable[$key].Count -eq 0)
+            {
+                break;
+            }
+
+            foreach($item in $this._FilterTable[$key])
+            {
+                if ($item -eq $value)
+                {
+                    return $Script:bRet
+                }
+            }
+            
+            $Script:bRet = $false;
+
+        } while ($false);
+
+        return $Script:bRet;
+    }
+
+    DumpFilter()
+    {
+        foreach($eachItem in $this._FilterTable.keys)
+        {
+            Write-Host $eachItem,$this._FilterTable[$eachItem]
+        }
+    }
+
+    AddFilter([string]$key, $value)
+    {
+        if ($this._FilterTable[$key].Count -gt 0)
+        {
+            $this._FilterTable[$key]+=$value
+        }
+        else 
+        {
+            $this._FilterTable[$key] = @( $value)
+        }
+    }
+
+    [string] GetLogFile()
+    {
+        if ($this._FilterTable["Logfile"])
+        {
+            return $this._FilterTable["Logfile"];
+        }
+
+        return "*";
+    }
 }
 
 class CNtLogEvent
 {
-    [System.Object]$_NtLogEvent;
-    [scriptblock]$_Callback;
- 
+    hidden [System.Object]$_NtLogEvent = $null;
+    hidden [scriptblock]$_EnumChildCallback = $null;
+    hidden [scriptblock]$_EnumChildPropertyCallback = $null;
+    hidden [CNtLogEventFilter]$_Filter;
+    hidden [int]$_nQueryLimit=100;
+    hidden [string]$_strLocalFile="*"
+    hidden [bool]$_bNeedLogPropery = $false;
+
     CNtLogEvent()
     {
-        $this._NtLogEvent = Get-CimInstance -Class Win32_Group
+
     }
 
-    SetCallback([scriptblock] $callback)
+    [System.Object]GetInstance()
     {
-        $this._Callback = $callback;
+        if ($null -eq $this._NtLogEvent)
+        {
+            $Script:strLogFile = $this.GetLogFile()
+
+            $this._NtLogEvent =  Get-CimInstance -Class Win32_NtLogEvent -Namespace "root\CIMV2" | 
+            Where-Object{ $_.Logfile -like $Script:strLogFile } | 
+            Select-Object -First $this._nQueryLimit
+        }
+
+        return $this._NtLogEvent
+    }
+
+    SetEnumChildCallback([scriptblock] $callback)
+    {
+        $this._EnumChildCallback = $callback;
+    }
+
+    SetEnumChildPropertyCallback([scriptblock] $callback)
+    {
+        $this._EnumChildPropertyCallback = $callback;
     }
     
-    EnumChild()
+    SetFilter([CNtLogEventFilter] $filter)
     {
-        for ($index = 0; $index -lt $this._NtLogEvent.Count; $index++ )
-        {
-            $_NtLogEventItem =  $this._NtLogEvent.Get($index)
+        $this._Filter = $filter;
+    }
 
-            if ($this._Callback)
+    [string] GetLogFile()
+    {
+        if ($this._strLocalFile -ne "*")
+        {
+            return $this._strLocalFile;
+        }
+
+        if ($this._Filter)
+        {
+            return $this._Filter.GetLogFile();
+        }
+
+        return $this._strLocalFile;
+    }
+
+    SetLogFile([string]$strLogFile)
+    {
+        $this._strLocalFile = $strLogFile
+    }
+
+    SetQueryLimit([int]$limit)
+    {
+        $this._nQueryLimit = $limit
+    }
+
+    [bool]EnumChildObject([System.Object] $object)
+    {
+        if ($this._EnumChildCallback)
+        {
+            $this._EnumChildCallback.Invoke($object)
+            return $true;
+        }
+
+        return $this.EnumChildProperty($object);
+    }
+
+    [bool]CheckFilter([System.Object] $object)
+    {
+        if ($this._Filter)
+        {
+
+            foreach($item1 in $object.CimInstanceProperties)
             {
-                $this._Callback.Invoke($_NtLogEventItem)
+                if ($this._Filter.IsTarget($item1) -eq $false)
+                {
+                    return $false;
+                }
+            }
+
+            foreach($item2 in $object.CimClass.CimClassQualifiers)
+            {
+                if ($this._Filter.IsTarget($item2) -eq $false)
+                {
+                    return $false;
+                }
             }
         }
+
+        return $true;
     }
+
+    [int]EnumChild()
+    {
+        $Script:nCount = 0;
+        foreach ($_NtLogEventItem in $this.GetInstance()) 
+        {
+            if ($this.EnumChildObject($_NtLogEventItem))
+            {
+                $Script:nCount++;
+            }
+        }
+        return $Script:nCount;
+    }
+
+    AnalysisChildPerProperty([System.Object] $obj)
+    {
+        if ($this._bNeedLogPropery)
+        {
+            Write-Host "Name:",$obj.Name, "; Value:",$obj.Value
+        }
+    }
+
+    SetNeedLogProperty([bool] $bValue)
+    {
+        $this._bNeedLogPropery = $bValue
+    }
+
+    [bool]EnumChildProperty([System.Object] $obj)
+    {
+        if ($this._EnumChildPropertyCallback)
+        {
+            $this._EnumChildPropertyCallback.Invoke($obj)
+            return $true;
+        }
+
+        if ($this.CheckFilter($obj) -eq $false)
+        {
+            return $false;
+        }
+        
+        foreach($item1 in $obj.CimInstanceProperties)
+        {
+            $this.AnalysisChildPerProperty($item1);
+        }
+
+        foreach($item2 in $obj.CimClass.CimClassQualifiers)
+        {
+            $this.AnalysisChildPerProperty($item2);   
+        }
+
+        return $true;
+    }
+
 }
 
+function MainEntry()
+{
+    $Script:ntLogFilter = [CNtLogEventFilter]::new()
+    $Script:ntLogFilter.AddFilter("Type", "ÐÅÏ¢")
+    $Script:ntLogFilter.AddFilter("Type", "´íÎó")
+    # $Script:ntLogFilter.DumpFilter()
 
-$myClass = [CNtLogEvent]::new()
-$myClass.SetCallback(${Function::Text})
-$myClass.EnumChild()
+    $Script:myClass = [CNtLogEvent]::new()
+    $Script:myClass.SetQueryLimit(1000)
+    $Script:myClass.SetFilter($Script:ntLogFilter)
+    $Script:myClass.SetLogFile("System")
+
+    $Script:nDealCount = $Script:myClass.EnumChild()
+
+    Write-Host "DealChildCount=",$Script:nDealCount
+}
+
+MainEntry
